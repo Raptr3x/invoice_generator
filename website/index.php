6a3be0add0e9d0d0b2085cc942c11267c3d3aa09
Bogdan Caleta Ivkovic <bogdan.caleta@gmail.com>
2022-09-19 19:08:30 +0200

    add login input fade

---
+++ website/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="assets/style.css">
+	<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+	<title>Login | Invoicey</title>
+</head>
+<body>
+	<div class="wrapper">
+		<div class="container">
+			<h1 class="logged-in">Login</h1>
+			<h1 class="hidden">Welcome!</h1>
+			
+			<form class="form" action="#">
+				<input type="text" placeholder="E-mail">
+				<input type="password" placeholder="Password">
+				<button type="submit" id="login-button">Login</button>
+			</form>
+
+			<p class="logged-in">Don't have an account?</p>
+			<a class="logged-in" href="register.php">Create one now!</a>
+		</div>
+		
+		<ul class="bg-bubbles">
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</div>
+	<script src="assets/main.js"></script>
+</body>
+</html>
